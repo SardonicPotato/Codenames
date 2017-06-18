3e7551ead1fb432671b26c93939742b88d8fc400
Andy <None>
2017-06-18 12:48:15 +0100

    Add default location for data directory.

--- web-server/scripts/generate_model_data.py
+++ web-server/scripts/generate_model_data.py
@@ -2,16 +2,19 @@ import sys, os
 from subprocess import call
 from solver.util import generated_reduced_model
 
-data_dir = sys.argv[1]
+try:
+    data_dir = sys.argv[1]
+except IndexError:
+    data_dir = "../data"
 
-print ("Downloading word2vec model.")
+print("Downloading word2vec model.")
 call(["wget", "-c", "-P", data_dir, "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"])
-print ("Unzipping model.")
+print("Unzipping model.")
 call(["gzip", "-d", os.path.join(data_dir, "GoogleNews-vectors-negative300.bin.gz")])
-print ("Generating reduced model embedding.")
+print("Generating reduced model embedding.")
 generated_reduced_model(input_path=os.path.join(data_dir, "GoogleNews-vectors-negative300.bin"),
                         output_dir=data_dir,
                         reduced_size=100000)
-print ("Cleaning up.")
+print("Cleaning up.")
 call(["rm", "-f", os.path.join(data_dir, "GoogleNews-vectors-negative300.bin.gz")])
 call(["rm", "-f", os.path.join(data_dir, "GoogleNews-vectors-negative300.bin")])
