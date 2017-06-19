3bccc4700877e2f8c8346d6b15811a2aa5fcfe17
Andy <None>
2017-06-19 17:50:54 +0100

    Uncomment lines.

--- web-server/create_embeddings.sh
+++ web-server/create_embeddings.sh
@@ -1,9 +1,9 @@
 DATA_DIR=data
 
 echo Downloading word2vec model
-#wget -P $DATA_DIR https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz
+wget -P $DATA_DIR https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz
 echo Unzipping model
-#gzip -d $DATA_DIR/GoogleNews-vectors-negative300.bin.gz
+gzip -d $DATA_DIR/GoogleNews-vectors-negative300.bin.gz
 echo Generating reduced model embedding
 python scripts/generate_reduced_model.py $DATA_DIR
 echo Cleaning up
